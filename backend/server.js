44fab2df5707e88d2a3a11a3ad545b83ddb9726e
tmitric996 <tmitric996@gmail.com>
2023-07-29 13:35:30 +0200

    registration form register user, fix modal

--- backend/server.js
+++ backend/server.js
@@ -3,22 +3,12 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const routes = require('./routes/routes');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors({ origin: 'http://localhost:8080' }));
 app.use('/', routes);
-
-const USERS_FILE = 'data/users.json';
-
-function getUsers() {
-    const usersData = fs.readFileSync(USERS_FILE);
-    return JSON.parse(usersData);
-}
-
-function saveUsers(users) {
-    const data = JSON.stringify(users);
-    fs.writeFileSync(USERS_FILE, data);
-}
 
 const PORT = 3000;
 app.listen(PORT, () => {
--- frontend/src/components/RegistrationForm.vue
+++ frontend/src/components/RegistrationForm.vue
@@ -2,36 +2,172 @@
   <div class="container mt-5">
     <div class="card p-4 shadow-lg">
       <h2 class="mb-4 text-center">Registration Form</h2>
-      <form @submit="registerUser">
+      <form @submit.prevent="registerUser">
         <div class="mb-3">
           <label for="username" class="form-label">Username</label>
           <input type="text" class="form-control" v-model="username" id="username" required>
         </div>
         <div class="mb-3">
           <label for="password" class="form-label">Password</label>
-          <input type="password" class="form-control" v-model="password" id="password" required>
+          <div class="input-group">
+            <input type="password" class="form-control" v-model="password" id="password" required>
+            <button
+                type="button"
+                class="btn btn-outline-secondary"
+                @click="togglePasswordVisibility('password')"
+                data-bs-toggle="tooltip" :title="showPassword ? 'Show password' : 'Hide password'"
+            >
+              <i :class="showPassword ? 'bi bi-eye-fill' : 'bi bi-eye-slash-fill'"></i>
+            </button>
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="confirmPassword" class="form-label">Confirm Password</label>
+          <div class="input-group">
+            <input
+                type="password"
+                class="form-control"
+                :class="{ 'is-invalid': passwordMismatch }"
+                v-model="confirmPassword"
+                id="confirmPassword"
+                required
+            >
+            <button
+                type="button"
+                class="btn btn-outline-secondary"
+                @click="togglePasswordVisibility('confirmPassword')"
+                data-bs-toggle="tooltip" :title="showConfirmPassword ? 'Show password' : 'Hide password'"
+            >
+              <i :class="showConfirmPassword ? 'bi bi-eye-fill' : 'bi bi-eye-slash-fill'"></i>
+            </button>
+            <div v-if="passwordMismatch" class="invalid-feedback">
+              Passwords do not match.
+            </div>
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="firstName" class="form-label">First Name</label>
+          <input type="text" class="form-control" v-model="firstName" id="firstName" required>
+        </div>
+        <div class="mb-3">
+          <label for="lastName" class="form-label">Last Name</label>
+          <input type="text" class="form-control" v-model="lastName" id="lastName" required>
+        </div>
+        <div class="mb-3">
+          <label for="gender" class="form-label">Gender</label>
+          <select class="form-select" v-model="gender" id="gender" required>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <div class="mb-3">
+          <label for="dateOfBirth" class="form-label">Date of Birth</label>
+          <input type="date" class="form-control" v-model="dateOfBirth" id="dateOfBirth" required>
         </div>
         <div class="d-grid gap-2 mt-4">
-          <button type="submit" class="btn btn-primary">Register</button>
+          <button type="submit" class="btn btn-primary" :disabled="passwordMismatch">Register</button>
         </div>
       </form>
+    </div>
+    <div :class="isModalShown? 'modal fade show' : 'modal fade'" id="registrationModal" ref="registrationModal" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" v-if="registrationStatus === 'success'">Registration Successful</h5>
+            <h5 class="modal-title" v-else-if="registrationStatus === 'error'">Registration Error</h5>
+          </div>
+          <div class="modal-body">
+            <p v-if="registrationStatus === 'success'">Congratulations! You have successfully registered.</p>
+            <p v-else-if="registrationStatus === 'error'">Oops! Something went wrong during registration.</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" @click="closeModal">Close</button>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import '../../public/assets/styles.css';
+import axios from 'axios';
 
 export default {
   data() {
     return {
+      showPassword : true,
+      showConfirmPassword : true,
       username: '',
       password: '',
+      confirmPassword: '',
+      firstName: '',
+      lastName: '',
+      gender: 'male',
+      dateOfBirth: '',
+      registrationStatus: null,
+      isModalShown: false,
     };
   },
+  computed: {
+    passwordMismatch() {
+      return this.password !== this.confirmPassword;
+    },
+  },
   methods: {
-    registerUser() {
-     },
+    async registerUser() {
+      if (this.passwordMismatch) {
+        return;
+      }
+      try {
+        const response = await axios.post('http://localhost:3000/register', {
+          username: this.username,
+          password: this.password,
+          firstName: this.firstName,
+          lastName: this.lastName,
+          gender: this.gender,
+          dateOfBirth: this.dateOfBirth
+        });
+
+        if (response.status === 200) {
+          this.registrationStatus = 'success';
+          console.log('Korisnik je uspešno registrovan.');
+        } else {
+          this.registrationStatus = 'error';
+          console.log('Došlo je do greške prilikom registracije korisnika.');
+        }
+        this.showModal();
+        return;
+
+      } catch (error) {
+        console.error('Greška prilikom slanja zahteva:', error);
+      }
+    },
+    togglePasswordVisibility(field) {
+      if (field=='password') {
+        this.showPassword = !this.showPassword;
+      } else {
+        this.showConfirmPassword = !this.showConfirmPassword;
+      }
+      const input = document.getElementById(field);
+      if (input.type === 'password') {
+        input.type = 'text';
+      } else {
+        input.type = 'password';
+      }
+    },
+    showModal() {
+      const modal = new bootstrap.Modal(this.$refs.registrationModal);
+      modal.show();
+      this.isModalShown=true;
+    },
+
+    closeModal() {
+      const modal = new bootstrap.Modal(this.$refs.registrationModal);
+      modal.hide();
+      this.isModalShown=false;
+    },
   },
 };
 </script>
